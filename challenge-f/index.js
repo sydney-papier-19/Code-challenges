86bfbb9ebaac904afb53963306f3cca50b5f150f
sydney_p <sydneypapier23@gmail.com>
2024-09-04 11:07:17 +0200

    changed count,year,status to let

---
+++ challenge-f/index.js
@@ -0,0 +1,39 @@
+let year= 2050;
+let status= 'student';
+let count = 0;
+
+if (year === 2050) {
+	console.log("January", 'New Year’s Day')
+	console.log("March", 'Human Rights Day')
+	let date = 'April';
+	console.log(date, 'Family Day')
+	console.log(date, 'Freedom Day')
+	 count += 4;
+
+	if (status === 'student') {
+	  console.log('June', 'Youth Day')
+		  count += 1;
+  }
+
+	console.log('August', 'Women’s Day')
+	console.log('September', 'Heritage Day')
+	date = 'December';
+	console.log(date, 'Day of Reconciliation')
+	  count += 3;
+
+	if (status = 'parent') {
+	  console.log(date, 'Christmas Day')
+		count += 1;
+  }
+
+	console.log(date, 'Day of Goodwill')
+	 count = count += 1;
+}
+
+console.log('Your status is:', status)
+console.log('The year is:', year )
+console.log('The total holidays is:', count);
+
+let message= 'status' + " " + 'year' + 'count ';
+display= message;
+console.log(display);
