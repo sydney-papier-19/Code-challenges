74c65ecbe9835ff9afe4418c1ce889b72eaa91f0
sydney_p <sydneypapier23@gmail.com>
2024-09-11 12:05:11 +0200

    debugged None selected and added variables

--- challenge-g/index.js
+++ challenge-g/index.js
@@ -2,42 +2,51 @@ let FREE_WARNING = 'Free shipping only applies to single customer orders'
 let BANNED_WARNING = 'Unfortunately we do not ship to your country of residence'
 let NONE_SELECTED = '0'
 
-const RSA= 'RSA'
-const NAM= 'NAM'
-const NK= 'NK'
-
 let location = 'RSA';
 let shipping= null;
 let customers= 1;
-let currency= 'R';
+let currency= null;
 
-const shoes = 300 * 1
-const toys = 100 * 5
-const shirts = 150 * 'NONE_SELECTED'
-const batteries= 35 * 2
-const pens = 5 * 'NONE_SELECTED' 
+const shoes = 300 * 1;
+const toys = 100 * 5;
+const shirts = 150 * NONE_SELECTED;
+const batteries= 35 * 2;
+const pens = 5 * NONE_SELECTED;
 
 
-if (  location === RSA) { 
-    shipping === 400 && currency === 'R' 
-} else if (location = NAM) {
-shipping = 600 && currency==='$'
+if (  location === 'RSA') { 
+    shipping = 400 ;
+	currency = 'R' ;
+} else if (location === NAM) {
+shipping = 600 ;
+ currency='$';
 } else {
-    shipping = 800 && currency==='$'
-};
-
-if ( shoes + batteries + pens + shirts >= 1000 ) {
-	if (location === NAM && customers === 1) {
-			if (location = RSA)
-		    shipping = 0 || calcShipping
-		}
-	}
+    shipping = 800 ;
+	 currency='$';
+}
 
 
-if ( shipping === 0 && customers !== 1 ) { console.log(FREE_WARNING) };
+const total = shoes + batteries + pens + shirts + toys;
+console.log()
+const isFreeLocal = currency === 'R' && total > 1000;
+const isFreeInt = currency === '$' && total > 60;
+const isFree = isFreeLocal || isFreeInt;
+const isFreeWarning = isFree && customers !== 1;
+const totalWithShipping = total + shipping;
+const isBanned = location === 'NK';
 
-location = 'NK' ? console.log(BANNED_WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping);
+if ( isFreeWarning ) {
+      console.log(FREE_WARNING);
+}
 
-customers = '1'
-location = 'RSA'
-currency = null
+if ( isFree && !isFreeWarning) {
+	shipping = 0;
+}
+
+// if ( isBanned){
+// 	console.log(BANNED_WARNING);
+// } else {
+// 	console.log( 'price'+currency + totalWithShipping);
+// }
+console.log(isBanned? BANNED_WARNING : 'price: '+ currency + totalWithShipping);
+ 
