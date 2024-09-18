d4ef2c99f6ab5f3298ce647e238a8feae3bc8b29
sydney_p <sydneypapier23@gmail.com>
2024-09-18 12:09:11 +0200

    debugged  if condition further and const's inside

--- challenge-i/index.js
+++ challenge-i/index.js
@@ -3,16 +3,22 @@ const tax = '8%';
 const food = 51.7501;
 const salary = 800;
 const transport = 10.2;
-const hourOfDay = 00;
-const minuteOfDay = 00;
+const hourOfDay = '00';
+const minuteOfDay = '00';
 
 // Only change below this line
 
-if  (hourOfDay === '00' && minuteOfDay === '00') {
-	const taxAsDecimal = tax / '100';
-    const startingAfterTax = salary * '1' - taxAsDecimal;
+if  (Number(hourOfDay) === 0 && Number(minuteOfDay) === 0) {
+	const taxAsDecimal = tax ;
+    
+    const startingAfterTax = salary - taxAsDecimal;
+    console.log(startingAfterTax)
+    
 	const balance = startingAfterTax - transport - food - rent;
-} else if ()
+    console.log(balance)
+} 
+
+
 	
 console.log('Your balance is: R' + balance.toFixed(2));
 
