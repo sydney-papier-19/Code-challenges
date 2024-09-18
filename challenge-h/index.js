f1127e7bd3b68565175340079de744a778af6a76
sydney_p <sydneypapier23@gmail.com>
2024-09-18 09:33:21 +0200

    added .test() and commenting

--- challenge-h/index.js
+++ challenge-h/index.js
@@ -3,10 +3,12 @@ const primaryPhone = '0748105141'
 const secondaryPhone = '0219131568'
 
 // Only change below this line
+//Regular expression used to check for numbers 
  const numberPattern = /^\d+$/;
 
-const primaryValid = typeof number == primaryPhone 
-const secondaryValid = typeof number == secondaryPhone
+ //.test(primaryPhone) checks if the phone number strings matches the regex 
+const primaryValid = numberPattern.test(primaryPhone) 
+const secondaryValid = numberPattern.test(secondaryPhone)
 
 console.log('Primary phone is valid numerical string:', primaryValid)
 console.log('Secondary phone is valid numerical string:', secondaryValid )
