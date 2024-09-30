4b0b725760bf88ca404eb3aa5ced456f5ad4e08a
sydney_p <sydneypapier23@gmail.com>
2024-09-30 14:47:38 +0200

    linked chall-k html and js, debugged and added validation

--- challenge-k/index.js
+++ challenge-k/index.js
@@ -2,4 +2,9 @@
 const nickname= "Timmy";
 const firstname = "Timothy";
 
-console.log("Good Morning, ${nickname} || {firstname}!")
+//removed "", replaced it with template literals ``
+console.log(`Good Morning, ${nickname} || ${firstname}!`)
+
+if ( firstname === null && nickname === null ) {
+    console.log("Good morning!");
+}
