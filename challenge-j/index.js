2c8212737224838756585c43c95d8ed5f0f50305
sydney_p <sydneypapier23@gmail.com>
2024-09-30 14:35:36 +0200

    solved challenge-j by removing string

--- challenge-j/index.js
+++ challenge-j/index.js
@@ -1,3 +1,9 @@
 
-const value = "3"
+//const value = "3";
+
+//Used Number() to convert string of "3" to a number
+//console.log( Number(value) + 4 + Number(value ));
+
+//Changed string "3" to a number by removing ""
+const value = 3;
 console.log(value + 4 + value)
